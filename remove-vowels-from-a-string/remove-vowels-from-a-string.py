dc11799f8082eaf09ddd06019192a86660049e22
Karan M Rajpal <rajpal.karan73@gmail.com>
2021-02-01 10:21:54 -0600

    Time: 44 ms (9.37%), Space: 14.3 MB (50.77%) - LeetHub

--- remove-vowels-from-a-string/remove-vowels-from-a-string.py
+++ remove-vowels-from-a-string/remove-vowels-from-a-string.py
@@ -2,13 +2,30 @@ import re
 
 class Solution:
     def removeVowels(self, s: str) -> str:
-        # old approach
+        # approach 1
         # vowelsStripped = re.findall(r"[^aeiou]", s)
         # return "".join(vowelsStripped)
         
-        vowelsStripped = []
-        for char in s:
-            if char not in ['a', 'e', 'i', 'o', 'u']:
-                vowelsStripped.append(char)
+        # Approach 2        
+        # vowelsStripped = []
+        # for char in s:
+        #   if char not in ['a', 'e', 'i', 'o', 'u']:
+        #       vowelsStripped.append(char)
                 
-        return "".join(vowelsStripped)
+        # return "".join(vowelsStripped)
+        
+        
+        # Approach 3
+        vowelSet = set()
+        vowelSet.add('a')
+        vowelSet.add('e')
+        vowelSet.add('i')
+        vowelSet.add('o')
+        vowelSet.add('u')
+        
+        vowelsStripped = ""
+        for char in s:
+            if char not in vowelSet:
+                vowelsStripped += char
+        
+        return vowelsStripped
