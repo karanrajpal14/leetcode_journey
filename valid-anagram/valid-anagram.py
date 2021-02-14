3de073d3b1bfdc7f51dddbdf10b80f73f833d14c
Karan M Rajpal <rajpal.karan73@gmail.com>
2021-02-13 18:23:59 -0600

    Time: 32 ms (97.68%), Space: 14.4 MB (74.92%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,28 +1,37 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Approach 1 - From scratch
+        # Sanitization
         s = s.replace(" ", "").lower()
         t = t.replace(" ", "").lower()
         
+        # Edge case check
         if len(s) != len(t):
             return False
         
-        count = {}
+        # Approach 1 - From scratch
         
-        for char in s:
-            if char in count:
-                count[char] += 1
-            else:
-                count[char] = 1
+        # count = {}
         
-        for char in t:
-            if char in count:
-                count[char] -= 1
-            else:
-                count[char] = 1
+        # for char in s:
+        #     if char in count:
+        #         count[char] += 1
+        #     else:
+        #         count[char] = 1
         
-        for i in count:
-            if count[i] != 0:
-                return False
+        # for char in t:
+        #     if char in count:
+        #         count[char] -= 1
+        #     else:
+        #         count[char] = 1
+        
+        # for i in count:
+        #     if count[i] != 0:
+        #         return False
                 
-        return True
+        # return True
+        
+        # Approach 2 - Using Counter
+        return Counter(s) == Counter(t)
+        
